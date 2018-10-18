6cbf20eb9ed4a746af0f6eecd13ea29df32df7ed
U-ITEC\is40701 <is40701@e4-is40701-1.itec.local>
2018-10-18 11:36:46 +0900

    aaaa

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,7 +2,27 @@ package example
 
 object Hello extends Greeting with App {
   println(greeting)
-  
+  println("aaaaaa")
+  // var と val
+  var num:Int = 10
+  num = 20
+  var n1,n2,n3 = 1
+  val msg = "日本語でOK"
+  // valは再代入を許さない
+  // 基本的にvalを使っていくべし
+  //配列
+  val a1 = new Array[Int](10)
+  a1(1) = 2
+  // リスト(イミュータブル)
+  val list1 = List("Scala")
+  println(list1)
+  // マップ(イミュータブル)
+  val map1 = Map[String,Int]("S"-> 1,"B"->3)
+  // タプル
+  val t3 = ("S","B",1)
+  println(t3._2)
+
+  lazy val lazyX = {println("lazy int val");num + 1}
 }
 
 trait Greeting {
