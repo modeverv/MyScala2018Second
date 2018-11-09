80e29cdc236cfcb677d7072ffce9e6e2f29a04b3
U-ITEC\is40701 <is40701@e4-is40701-1.itec.local>
2018-11-09 15:05:28 +0900

    2018-11-09 15:05:26

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -38,6 +38,25 @@ object Hello extends Greeting with App {
   for(item <- listItems if item.length > 4) println("item = " + item)
   val resultList = for(item <- listItems) yield "I use " + item
   println(resultList)
+  // def 関数名[方パラメーター](引数名:型):戻り値 = 関数本体
+  def add(x:Int,y:Int) :Int = x+ y
+  println(add(1,2))
+  def p(x:Any) = println(x)
+  p("aaaaa")
+
+  val func = (x:Int,y:Int) => x + y
+  val func2 :(Int,Int) => Int = (x:Int,y:Int) => x + y
+  func(1,3)
+  // 関数を引数にとる
+  def calc(f:(Int,Int) => Int,num:Int) :Int = f(num,num)
+  var fxy = calc((x,y) => x+ y,10)
+  p(fxy)
+  p(calc(add,10))
+  // 高階関数/メソッド
+  def getFunc(str:String) : (Int,Int) => String = (x:Int,y:Int) => str + (x + y)
+  var f = getFunc("results") // 関数をもらう
+  p(f(1,2))
+  p(getFunc("hoge")(2,3))
 }
 
 trait Greeting {
